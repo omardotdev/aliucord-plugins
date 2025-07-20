88d260aa7c9d7a8f8bd2c74055b9a3ad92ac5143
Omar <180954465+omardotdev@users.noreply.github.com>
2025-07-21 01:21:45 +0300

    fix: typo, im on mobile :|

--- plugins/kotlin/minky/src/main/kotlin/com/github/omardotdev/minky.kt
+++ plugins/kotlin/minky/src/main/kotlin/com/github/omardotdev/minky.kt
@@ -3,7 +3,7 @@
  * Copyright (C) 2025 Omar
  *
  * This plugin was taken from Wolfkid200444 and modfiied.
- * The original plugin's name is petoet
+ * The original plugin's name is petpet
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
