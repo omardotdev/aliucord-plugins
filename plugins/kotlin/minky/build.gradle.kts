96e119dd8efa8b5c9e6b3ba71cb7b8bb38e410c5
Omar <om749843@tutanota.de>
2025-07-23 16:26:46 +0300

    feat(minky): rewrite

--- plugins/kotlin/minky/build.gradle.kts
+++ plugins/kotlin/minky/build.gradle.kts
@@ -1,9 +1,9 @@
-version = "1.0.1"
+version = "1.1.0"
 description = "sends a random picture of minky :3 (zt64's cat)"
 
 aliucord {
     changelog.set("""
-        first version
+        rewrite the code
     """.trimIndent())
 
     excludeFromUpdaterJson.set(false)
--- plugins/kotlin/minky/src/main/kotlin/com/github/omardotdev/minky.kt
+++ plugins/kotlin/minky/src/main/kotlin/com/github/omardotdev/minky.kt
@@ -2,16 +2,13 @@
  * Omar (omardotdev)'s Aliucord Plugins
  * Copyright (C) 2025 Omar
  *
- * This plugin was taken from Wolfkid200444 and modfiied.
- * The original plugin's name is petpet
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at 
  * http://www.apache.org/licenses/LICENSE-2.0
 */
 
-package com.aliucord.plugins
+package com.minky.minky
 
 import android.content.Context
 import android.net.Uri
@@ -23,44 +20,30 @@ import com.aliucord.api.CommandsAPI.CommandResult
 import com.aliucord.entities.CommandContext
 import com.aliucord.entities.Plugin
 import com.discord.api.commands.ApplicationCommandType
-import com.discord.utilities.icon.IconUtils
 import java.io.File
 import java.io.FileOutputStream
 
 @AliucordPlugin
 class Minky : Plugin() {
-    override fun start(context: Context) {
-        commands.registerCommand(
-            "minky",
-            "Send an image of Minky",
-    	) 
+    override fun start(c: Context) {
+        commands.registerCommand("minky", "Send an image of Minky") 
 	{ ctx: CommandContext ->
-            var file: File? = null
-            try {
-                file = imageToDataUri(context)
-            } catch (e: Throwable) {
-                Main.logger.error(e)
-            }
-            assert(file != null)
+            var file = getImage(c)
             ctx.addAttachment(Uri.fromFile(file).toString(), "minky.jpg")
             CommandResult("")
         }
     }
 
-    @Throws(Throwable::class)
-    private fun imageToDataUri(mContext: Context): File {
+    private fun getImage(c: Context): File {
+	var url = "https://minky.materii.dev"
         val res = Http.Request(url).execute()
-        val f = File.createTempFile("temp", ".jpg", mContext.cacheDir)
-        FileOutputStream(f).use { fos -> res.pipe(fos) }
-        f.deleteOnExit()
-        return f
+        val file = File.createTempFile("temp", ".jpg", c.cacheDir)
+        FileOutputStream(file).use { fos -> res.pipe(fos) }
+        file.deleteOnExit()
+        return file
     }
 
     override fun stop(context: Context) {
         commands.unregisterAll()
-    }
-
-    companion object {
-        private const val url = "https://minky.materii.dev/"
     }
 }
