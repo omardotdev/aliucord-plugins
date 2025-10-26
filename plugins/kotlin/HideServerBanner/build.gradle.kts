0c50f6bc44a9f716d0808b8ff4a5993faea42d8f
Omar <180954465+omardotdev@users.noreply.github.com>
2025-10-26 07:33:41 +0300

    bump version

--- plugins/kotlin/HideServerBanner/build.gradle.kts
+++ plugins/kotlin/HideServerBanner/build.gradle.kts
@@ -1,9 +1,9 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Hides the server banner from the channel list"
 
 aliucord {
     changelog.set("""
-        adjust name
+        some fixes
     """.trimIndent())
 
     excludeFromUpdaterJson.set(false)
