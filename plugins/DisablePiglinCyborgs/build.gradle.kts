5eeac967cdec58533078bde0b8b01dccce5ce8e7
Omar <om749499@tutanota.de>
2026-05-17 12:29:46 +0300

    feat(DisablePiglinCyborgs): inert

--- plugins/DisablePiglinCyborgs/build.gradle.kts
+++ plugins/DisablePiglinCyborgs/build.gradle.kts
@@ -1,8 +1,8 @@
-version = "1.0.1"
+version = "1.0.2"
 description = "disables known vibe coded/ai-assisted plugins"
 
 aliucord {
     changelog.set("""
-        add some more plugins!!
+        this plugin will do nothing anymore so uhhh please uninstall it
     """.trimIndent())
 }
--- plugins/DisablePiglinCyborgs/src/main/kotlin/com/github/omardotdev/DisablePiglinCyborgs.kt
+++ plugins/DisablePiglinCyborgs/src/main/kotlin/com/github/omardotdev/DisablePiglinCyborgs.kt
@@ -11,36 +11,12 @@
 package com.github.omardotdev
 
 import android.content.Context
-import com.aliucord.PluginManager.disablePlugin
-import com.aliucord.PluginManager.plugins
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 
 @AliucordPlugin
 class DisablePiglinCyborgs : Plugin() {
-    override fun start(context: Context) {
-        val piglins = listOf(
-            "ReviewDB",
-            "HeicImageConverter",
-            "PowerMode",
-            "OnboardingFix",
-            "AvatarResizer",
-            "PasswordLogin",
-            "FullscreenLock",
-            "BetterUserDetails"
-        )
-
-        for (piglin in piglins) {
-            if (plugins[piglin] == null) continue
-
-            try {
-                logger.info("⚠ potential ai assisted/vibe coded plugin detected, disabling")
-                disablePlugin(piglin)
-            } catch (e: Exception) {
-                logger.info("failed to disable ai assisted plugin!", e)
-            }
-        }
-    }
+    override fun start(context: Context) {}
 
     override fun stop(context: Context) {
         patcher.unpatchAll()
